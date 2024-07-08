83ca0bc1745adbdbd838542b458fb10105e173ec
Vladislav07v <whdfcr@gmail.com>
2024-07-08 11:06:47 +0300

    Моята част от проекта

---
+++ Parking.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingManagementSystrem
+{
+    internal class Parking
+    {
+        private int totalSpaces;
+        private int avaliableSpaces;
+        public string ParkingID { get; private set; }
+        public string Location { get; private set; }
+        public int TotalSpaces
+        {
+            get { return totalSpaces; }
+            private set
+            {
+                if (value <= 0) { throw new ArgumentException("Броят на всички парко места трябва да е положителен!"); }
+            }
+        }
+        public int AvaliableSpaces
+        {
+            get { return avaliableSpaces; }
+            private set
+            {
+                if (value <= 0) { throw new ArgumentException("Броят на наличните парко места трябва да е положителен!"); }
+            }
+        }
+        public string Vehicles { get; private set; }
+
+        public Parking(string parkingId, string location, int totalSpaces, int avaliableSpaces, string vehicles)
+        {
+            ParkingID = parkingId;
+            Location = location;
+            TotalSpaces = totalSpaces;
+            AvaliableSpaces = avaliableSpaces;
+            Vehicles = vehicles;
+        }
+        public override string ToString()
+        {
+            return $"{ParkingID},{Location},{TotalSpaces},{AvaliableSpaces},{Vehicles}";
+        }
+        public void DecreaseSeats(int ticketsCount)
+        {
+            if (AvaliableSpaces < TotalSpaces)
+            {
+                Console.WriteLine("Няма толкова налични места.");
+                return;
+            }
+            AvaliableSpaces -= TotalSpaces;
+            //todo: message to be print
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,185 @@
+﻿using ParkingManagementSystrem;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingManagementSystrem
+{
+    internal class Program
+    {
+        private const string filePath = "../../../parking.txt";
+
+        private static List<Parking> parkings = new List<Parking>();
+        private static string menuActionChoice;
+        static void Main(string[] args)
+        {
+            PrintMenu();
+
+            while (true)
+            {
+                menuActionChoice = Console.ReadLine();
+                switch (menuActionChoice)
+                {
+                    case "1":
+                        ShowActionTitle("Създаване на нов паркинг");
+                        ParkingID();
+                        break;
+                    case "2":
+                        ShowActionTitle("Местоположение на паркинга");
+                        ParkingLocation();
+                        break;
+                    case "3":
+                        ShowActionTitle("Брой паркоместа");
+                        TotalSpaces();
+                        break;
+                    case "4":
+                        ShowActionTitle("Налични паркоместа");
+                        AvaliableSpaces();
+                        break;
+                    case "5":
+                        ShowActionTitle("Регистрационен номер на колата");
+                        Vehicles();
+                        break;
+                    case "x" or "X":
+                        Exit();
+                        break;
+                    default:
+                        Console.WriteLine("Това на е налична опция");
+                        break;
+                }
+            }
+        }
+
+        private static void PrintMenu()
+        {
+            throw new NotImplementedException();
+            //За Митко
+        }
+
+        private static void ShowActionTitle(string title)
+        {
+            Console.Clear();
+            AddLine();
+            Console.WriteLine("\t" + title);
+            AddLine();
+        }
+
+        private static void Exit()
+        {
+            Environment.Exit(0);
+        }
+
+        private static void Vehicles()
+        {
+            throw new NotImplementedException();
+            //За Джан
+        }
+
+        private static void AvaliableSpaces()
+        {
+            throw new NotImplementedException();
+            //За Джан
+        }
+
+        private static void TotalSpaces()
+        {
+            throw new NotImplementedException();
+            //За Джан
+        }
+
+        private static void ParkingLocation()
+        {
+            throw new NotImplementedException();
+            //За Джан
+        }
+
+        private static void ParkingID()
+        {
+            throw new NotImplementedException();
+            //За Джан
+        }
+
+        private static void SaveParkings()
+        {
+            StreamWriter writer = new StreamWriter(filePath, false, Encoding.Unicode);
+            using (writer)
+            {
+                foreach (Parking parking in parkings)
+                {
+                    writer.WriteLine(parking);
+                }
+            }
+        }
+
+        private static void LoadParkings()
+        {
+            StreamReader reader = new StreamReader(filePath, Encoding.Unicode);
+            using (reader)
+            {
+                string line;
+                while ((line = Console.ReadLine()) != null)
+                {
+                    string[] parkingInfo = line.Split(',').ToArray();
+                    string parkingId = parkingInfo[0];
+                    string location = parkingInfo[1];
+                    int totalSpaces = int.Parse(parkingInfo[2]);
+                    int avaliableSpaces = int.Parse(parkingInfo[3]);
+                    string vehicles = parkingInfo[4];
+
+                    Parking currentParking = new Parking(parkingId, location, totalSpaces, avaliableSpaces, vehicles);
+                    parkings.Add(currentParking);
+                }
+            }
+        }
+        
+        private static void ListParkings()
+        {
+            foreach (Parking parking in parkings)
+            {
+                PrintParkingInfo(parking);
+                AddLine();
+            }
+
+            BackToMenu();
+        }
+
+        private static void BackToMenu()
+        {
+            throw new NotImplementedException();
+            //За Митко
+        }
+
+        private static void AddLine()
+        {
+            throw new NotImplementedException();
+            //За Митко
+        }
+
+        private static void PrintParkingInfo(Parking parking)
+        {
+            throw new NotImplementedException();
+            //За Митко
+        }
+
+        private static void ShowResultMessage(string message)
+        {
+            AddLine();
+            Console.WriteLine("\t" + message);
+        }
+
+        private static void AddNewParking()
+        {
+            throw new NotImplementedException();
+            //За Джан
+        }
+
+        private static void SearchParking()
+        {
+            throw new NotImplementedException();
+            //За Джан
+        }
+    }
+}
